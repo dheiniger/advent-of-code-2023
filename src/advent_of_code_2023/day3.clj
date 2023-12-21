88e7239bf5c7b38d8019fac7bacc927ab0e72082
Daniel <daniel.r.heiniger@gmail.com>
2023-12-21 12:49:05 -0600

    day 3

--- src/advent_of_code_2023/day3.clj
+++ src/advent_of_code_2023/day3.clj
@@ -3,82 +3,66 @@
             [clojure.string :as str]))
 
 (def test-input (util/read-lines "input/day3/test.txt"))
-(def input (util/read-lines "input/day3/input.txt"))
 
-(defn digit? [c]
-  (Character/isDigit c))
+(defn- get-indexed-numbers[line]
+  (let [indexed (map-indexed vector line)]
+    (->> indexed
+         (partition-by #(digit? (second %)))
+         (remove #(not (digit? (first (nfirst %))))))))
 
-(defn char-symbol? [c]
-  (and
-   (not= \. c)
-   (not (Character/isDigit c))))
+(defn- get-indexed-vals[line]
+  (map-indexed vector line))
 
-(defn- get-grid []
-  (for [[y line] (map-indexed (fn [idx item] [idx item]) input)
-        [x c]    (map-indexed (fn [idx item] [idx item]) line)]
-    {:y y :x x :val c}))
+(defn- make-full-grid [input]
+  (map get-indexed-vals input))
 
+(defn- make-num-grid [input]
+  (map get-indexed-numbers input))
 
-(defn- determine-search-coords [{:keys[x y] :as grid-point} grid]
-  (let [x-points #{(dec x) x (inc x)}
-        y-points #{(dec y) y (inc y)}]
-    (for [x_ x-points
-          y_ y-points
-          :let [val (->> grid (filter #(and (= (:y %1) y_) (= (:x %1 ) x_)));;TODO I think this can be optimized
-                         first
-                         :val)]
-          :when (and (not (nil? val))
-                     (not (and (= x x_)
-                               (= y y_))))]
-      {:x x_ :y y_ :val val})))
+(defn- get-coord-val [[x y] grid]
+  (try
+    (first (filter #(= x (first %)) (nth grid y)))
+    (catch IndexOutOfBoundsException e nil)))
 
-(defn- keep-val? [search-points]
-  (filter #(char-symbol? (:val %)) search-points))
+(defn- get-search-coords[x y]
+  [[(dec x) y]
+   [(inc x) y]
+   [(dec x) (dec y)]
+   [x (dec y)]
+   [(inc x) (dec y)]
+   [(dec x) (inc y)]
+   [x (inc y)]
+   [(inc x) (inc y)]])
 
-(defn- nums-with-adjacent-symbols[grid]
-  (for [grid-point grid
-        :let [check-val (:val grid-point)
-              search-coords (determine-search-coords grid-point grid)
-              contains-symbol? (some #(char-symbol? (:val %)) search-coords)]
-        :when (and (digit? check-val) contains-symbol?)]
-    grid-point))
+(defn- get-search-vals [search-coords grid]
+  (remove nil? (map #(get-coord-val % grid) search-coords)))
 
-(defn- get-num-groups[grid]
-  (->> grid;;TODO -ITERATION
-       (partition-by #(digit? (:val %)))
-       (map-indexed (fn[idx itm] [(inc idx) itm]))
-       (filter #(odd? (first %)))
-       (map second)))
+(defn- group-contains-symbol?
+  [num-group y-index full-grid]
+  (let [search-coords (partition 2 (flatten
+                                    (map #(get-search-coords (first %) y-index)
+                                         num-group)))
+        search-vals (get-search-vals search-coords full-grid)]
+    (some #(char-symbol? (second %)) search-vals)))
+    
+(defn- get-digits-to-keep [input]
+  (let [full-grid (make-full-grid input)
+        num-grid (map-indexed vector (make-num-grid input))]
+    (for [[y-index line] num-grid
+          num-group line
+          :let [group-has-symbol? (group-contains-symbol? num-group y-index full-grid)]
+          :when group-has-symbol?]
+      num-group)))
 
-(defn- get-digits-to-keep[grid]
-  (let [nums-with-adjacent-symbols (into #{} (nums-with-adjacent-symbols grid))]
-    (for [num-group (get-num-groups grid)
-          :let [num-set (into #{} num-group)]]
-      (when-not (empty? (clojure.set/intersection num-set nums-with-adjacent-symbols))
-        num-group))))
+(defn solve [input]
+  (let [digit-groups (get-digits-to-keep input)]
+    (->> digit-groups
+         (map (fn[group]
+                (->> group
+                     (map second)
+                     (apply str)
+                     (Integer/parseInt))))
+         (reduce +))))
 
-(defn get-nums-to-keep[grid]
-  (let [digits-to-keep (get-digits-to-keep grid)
-        ]
-    (for [nums digits-to-keep
-          :when (not (empty? nums))]
-      (->> nums
-           (map :val)
-           (apply str)
-           (Integer/parseInt)))))
-
-(defn solve[grid]
-  (reduce + (get-nums-to-keep grid)))
-(comment
-  (time (solve (get-grid)))
-  (solve (take 100 (get-grid)))
-  (time (nums-with-adjacent-symbols (get-grid)))
-  (time (get-digits-to-keep (get-grid)))
-  (get-nums-to-keep)
-  (nums-with-adjacent-symbols)
-  (map #(determine-search-coords % (get-grid)) (get-grid))
-  (first (get-grid))
-  (keep even? [1 2 3 4 5 6])
-
-  (last (take 1000 (get-grid))))
+(time (solve input))
 
