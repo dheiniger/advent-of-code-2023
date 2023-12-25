484fb4c0083859e9da74f8c250409db93dbd3c31
Daniel <daniel.r.heiniger@gmail.com>
2023-12-25 13:46:06 -0600

    Day3 part 2

--- src/advent_of_code_2023/day3.clj
+++ src/advent_of_code_2023/day3.clj
@@ -2,7 +2,18 @@
   (:require [advent-of-code-2023.util :as util]
             [clojure.string :as str]))
 
+(def input (util/read-lines "input/day3/input.txt"))
 (def test-input (util/read-lines "input/day3/test.txt"))
+
+input
+
+(defn digit? [c]
+  (Character/isDigit c))
+
+(defn char-symbol? [c]
+  (and
+   (not= \. c)
+   (not (Character/isDigit c))))
 
 (defn- get-indexed-numbers[line]
   (let [indexed (map-indexed vector line)]
@@ -21,18 +32,16 @@
 
 (defn- get-coord-val [[x y] grid]
   (try
-    (first (filter #(= x (first %)) (nth grid y)))
+    (first (map (fn[[x val]]
+                  [x y val])
+                (filter #(= x (first %)) (nth grid y))))
     (catch IndexOutOfBoundsException e nil)))
 
 (defn- get-search-coords[x y]
-  [[(dec x) y]
-   [(inc x) y]
-   [(dec x) (dec y)]
-   [x (dec y)]
-   [(inc x) (dec y)]
-   [(dec x) (inc y)]
-   [x (inc y)]
-   [(inc x) (inc y)]])
+  [[(dec x) y][(inc x) y]
+   [(dec x) (dec y)][x (dec y)]
+   [(inc x) (dec y)][(dec x) (inc y)]
+   [x (inc y)][(inc x) (inc y)]])
 
 (defn- get-search-vals [search-coords grid]
   (remove nil? (map #(get-coord-val % grid) search-coords)))
@@ -43,8 +52,9 @@
                                     (map #(get-search-coords (first %) y-index)
                                          num-group)))
         search-vals (get-search-vals search-coords full-grid)]
-    (some #(char-symbol? (second %)) search-vals)))
-    
+    (some #(char-symbol? (last %)) search-vals)))
+
+
 (defn- get-digits-to-keep [input]
   (let [full-grid (make-full-grid input)
         num-grid (map-indexed vector (make-num-grid input))]
@@ -64,5 +74,59 @@
                      (Integer/parseInt))))
          (reduce +))))
 
-(time (solve input))
+;;part2
 
+(defn- gear-symbol? [c]
+  (= \* c))
+
+(defn- num-group->integer [num-group]
+  (->> num-group
+       (map second)
+       (apply str)
+       (Integer/parseInt)))
+
+(defn- get-gear-pos [num-group y-index full-grid]
+  (let [search-coords (->> num-group
+                           (map #(get-search-coords (first %) y-index))
+                           (flatten)
+                           (partition 2))
+        gear-positions (filter #(gear-symbol? (last %))
+                               (get-search-vals search-coords full-grid))]
+    (set (map (fn[gear-pos]
+                {:xy [(first gear-pos) (second gear-pos)]
+                 :num-group [(num-group->integer num-group)]}) gear-positions))))
+
+(defn- find-gear-candidates[input]
+  (let [full-grid (make-full-grid input)
+        num-grid (map-indexed vector (make-num-grid input))]
+    (for [[y-index line] num-grid
+          num-group line
+          :let [gear-positions (get-gear-pos num-group y-index full-grid)]
+          :when (not (empty? gear-positions))]
+      gear-positions)))
+
+(defn- flatten-maps [&maps]
+  (reduce (fn[acc cand]
+            (concat acc cand))
+          &maps))
+
+(defn- find-gears [gear-candidates]
+  (let [ all-candidates  (flatten-maps gear-candidates)
+        all-candidates (reduce (fn[acc cand]
+                                 (if-let [existing (first (filter #(= (:xy cand) (:xy %)) acc))]
+                                   (replace {existing (update-in existing [:num-group] conj (first(:num-group cand)))} acc)
+                                   (conj acc cand)))
+                               #{}
+                               all-candidates)]
+    (filter #(= 2 (count (:num-group %))) all-candidates)))
+
+(defn solve-2 [input]
+  (let [gears (find-gears (find-gear-candidates input))]
+    (->> gears
+         (map :num-group)
+         (map #(apply * %))
+         (reduce +))))
+
+(comment
+  (time (solve input))
+  (time (solve-2 input))
